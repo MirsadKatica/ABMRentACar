ca19d0f6ab4e8e60d3d11f353a9cbea590db3b81
MirsadKat <mirsad.katica.atia@gmail.com>
2018-05-06 19:56:28 +0200

    implement feature:add admin panel add admin panel files create layout structure with partial files change admin picture add home controller with index action and view

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master_admin')
+@section('content')
+  <!-- Content Header (Page header) -->
+   <section class="content-header">
+      <h1>
+       Welcome Admin
+      </h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+      MOJ SADRZAJ
+
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route :: namespace('admin')->group(function(){
+	Route::get('/', 'HomeController@index');
 });
+
+
